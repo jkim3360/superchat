e5fc37689e0deb88fda05fc8198d1523d314d5d6
James Kim <jamesdaehokim@gmail.com>
2021-12-10 15:24:55 -0500

    add logic to display most recent room upon login

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import ChatMessage from './ChatMessage'
 
@@ -15,23 +15,48 @@ export default function ChatRoom() {
   const [rooms] = useCollectionData(roomsCollection.orderBy('createdAt'), {
     idField: 'id'
   })
-  console.log(rooms)
+
+  // firebase
+  //   .firestore()
+  //   .collection('rooms')
+  //   .get()
+  //   .then(snapshot => snapshot.docs.map(doc => console.log(doc.data())))
+
+  // firebase
+  // .firestore()
+  // .collection('events')
+  // .get()
+  // .then(snapshot => snapshot.docs.forEach(doc => console.log('snap shot: ',doc.data())))
+
   const mostRecentRoom = rooms && rooms[rooms.length - 1].id
-  console.log(mostRecentRoom)
+  
   //   get all users in room
   const [users] = useCollectionData(
-    roomsCollection.doc(mostRecentRoom).collection('users'),
+    roomsCollection
+      .doc(
+        // Ij7qZG4ZOGY8QTboDX2O
+        mostRecentRoom
+      )
+      .collection('users'),
     { idField: 'id' }
   )
   //   get all messages in room
   const [msgs] = useCollectionData(
-    roomsCollection.doc(mostRecentRoom).collection('messages'),
+    roomsCollection
+      .doc(
+        // Ij7qZG4ZOGY8QTboDX2O
+        mostRecentRoom
+      )
+      .collection('messages'),
     { idField: 'id' }
   )
 
-  console.log(users, msgs, mostRecentRoom)
-
-  const roomsRef = roomsCollection.doc(mostRecentRoom).collection('messages')
+  const roomsRef = roomsCollection
+    .doc(
+      // Ij7qZG4ZOGY8QTboDX2O
+      mostRecentRoom
+    )
+    .collection('messages')
   const roomsQuery = roomsRef.orderBy('createdAt').limit(25)
   const [messages] = useCollectionData(roomsQuery, { idField: 'id' })
   const [formValue, setFormValue] = useState('')
@@ -54,7 +79,6 @@ export default function ChatRoom() {
       <main>
         {messages &&
           messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-
         <span ref={dummy}></span>
       </main>
 
@@ -64,7 +88,6 @@ export default function ChatRoom() {
           onChange={e => setFormValue(e.target.value)}
           placeholder='New Message'
         />
-
         <button type='submit' disabled={!formValue}>
           🕊️
         </button>
