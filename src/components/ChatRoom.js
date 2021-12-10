ae34e30d25be854b05c1126bd0d73f311efc82f4
James Kim <jamesdaehokim@gmail.com>
2021-12-09 22:45:19 -0500

    get rooms, users and messages

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -5,59 +5,75 @@ import ChatMessage from './ChatMessage'
 import firebase from 'firebase/compat/app'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 
-export default function ChatRoom() {
-    const firestore = firebase.firestore()
-    const auth = firebase.auth()
-    const dummy = useRef()
-    const roomsRef = firestore
-      .collection('rooms')
-      .doc(
-        '48BiKt8bOStJS6SM8CvL'
-        //  || 'Ij7qZG4ZOGY8QTboDX2O'
-      )
-      .collection('messages')
-    const roomsQuery = roomsRef.orderBy('createdAt').limit(25)
-  
-    const [messages] = useCollectionData(roomsQuery, { idField: 'id' })
-  
-    const [formValue, setFormValue] = useState('')
-  
-    const sendMessage = async e => {
-      e.preventDefault()
-  
-      const { uid, photoURL } = auth.currentUser
-  
-      await roomsRef.add({
-        text: formValue,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        uid
-      })
-  
-      setFormValue('')
-      dummy.current.scrollIntoView({ behavior: 'smooth' })
-    }
-  
-    return (
-      <>
-        <main>
-          {messages &&
-            messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-  
-          <span ref={dummy}></span>
-        </main>
-  
-        <form onSubmit={sendMessage}>
-          <input
-            value={formValue}
-            onChange={e => setFormValue(e.target.value)}
-            placeholder='say something nice'
-          />
-  
-          <button type='submit' disabled={!formValue}>
-            🕊️
-          </button>
-        </form>
-      </>
-    )
+export default function ChatRoom({ rooms }) {
+  const firestore = firebase.firestore()
+  const auth = firebase.auth()
+  const dummy = useRef()
+  const roomsCollection = firestore.collection('rooms')
+  const { uid, photoURL } = auth.currentUser
+
+
+
+  let roomIds = useCollectionData(roomsCollection, { idField: 'id' })[0]
+
+console.log(roomIds)
+
+//   get all users in room
+  const [users] = useCollectionData(
+    roomsCollection.doc('dHakAIFWpGRNaOKNg2bb').collection('users'),
+    { idField: 'id' }
+  )
+
+//   get all messages in room
+  const [msgs] = useCollectionData(
+    roomsCollection.doc('dHakAIFWpGRNaOKNg2bb').collection('messages'),
+    { idField: 'id' }
+  )
+
+  console.log(users, msgs)
+
+  const roomsRef = roomsCollection
+    .doc('dHakAIFWpGRNaOKNg2bb')
+    .collection('messages')
+
+  const roomsQuery = roomsRef.orderBy('createdAt').limit(25)
+
+  const [messages] = useCollectionData(roomsQuery, { idField: 'id' })
+  const [formValue, setFormValue] = useState('')
+
+  const sendMessage = async e => {
+    e.preventDefault()
+
+    await roomsRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('')
+    dummy.current.scrollIntoView({ behavior: 'smooth' })
   }
-  
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={e => setFormValue(e.target.value)}
+          placeholder='say something nice'
+        />
+
+        <button type='submit' disabled={!formValue}>
+          🕊️
+        </button>
+      </form>
+    </>
+  )
+}
